9c57a0a78ca4bc631fdf9490acba42a4b5702386
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2023-11-13 15:32:53 -0300

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -23,16 +23,17 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.listen()
-    client_socket, address = server_socket.accept()
-    client_data = client_socket.recv(1024).decode('utf-8')
-    first_line = client_data.splitlines()[0]
-    path = first_line.split(" ")[1]
-    content = capture_final_path(unquote(path))
-    http_response = generate_content('1.1', '200 OK', 'text/plain', content)
-    print(http_response)
-    client_socket.sendall(http_response.encode())
-    client_socket.close()
+    while True:
+        server_socket.listen()
+        client_socket, address = server_socket.accept()
+        client_data = client_socket.recv(1024).decode('utf-8')
+        first_line = client_data.splitlines()[0]
+        path = first_line.split(" ")[1]
+        content = capture_final_path(unquote(path))
+        http_response = generate_content('1.1', '200 OK', 'text/plain', content)
+        print(http_response)
+        client_socket.sendall(http_response.encode())
+        client_socket.close()
 
 
 if __name__ == "__main__":
