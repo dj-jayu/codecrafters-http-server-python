d6e98762d3e73918f241e2c72e48d63230402f18
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2023-11-13 14:49:32 -0300

    pass 3rd stage

--- app/main.py
+++ app/main.py
@@ -1,5 +1,21 @@
 # Uncomment this to pass the first stage
 import socket
+import re
+from urllib.parse import unquote
+
+
+def generate_content(http_version, status_code, content_type, content):
+    # generates the header and body response
+    response_headers = [f'HTTP/{http_version} {status_code}', f'Content-Type: {content_type}',
+                        f'Content-Length: {len(content)}', content]
+    return '\r\n'.join(response_headers)
+
+
+def capture_final_path(path):
+    # receives an unquoted path and returns what is between <>
+    pattern = r'<(.*?)>'
+    findings = re.findall(pattern, path)
+    return findings[0]
 
 
 def main():
@@ -12,13 +28,10 @@ def main():
     client_data = client_socket.recv(1024).decode('utf-8')
     first_line = client_data.splitlines()[0]
     path = first_line.split(" ")[1]
-    if path == '/':
-        http_response = "HTTP/1.1 200 OK\r\n\r\n"
-    else:
-        http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    content = capture_final_path(unquote(path))
+    http_response = generate_content('1.1', '200 OK', 'text/plain', content)
     client_socket.sendall(http_response.encode())
     client_socket.close()
-
 
 
 if __name__ == "__main__":
