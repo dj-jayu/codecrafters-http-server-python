2316f3cdf0d2c72affdf51f82f9c834a0013325f
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2023-11-13 15:35:27 -0300

    pass 4th stage

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def generate_content(http_version, status_code, content_type, content):
     # generates the header and body response
     response_headers = [f'HTTP/{http_version} {status_code}', f'Content-Type: {content_type}',
                         f'Content-Length: {len(content)}', content]
-    return '\r\n'.join(response_headers)
+    return '\r\n'.join(response_headers) + '\r\n\r\n' + content
 
 
 def capture_final_path(path):
@@ -22,18 +22,18 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    while True:
-        server_socket.listen()
-        client_socket, address = server_socket.accept()
-        client_data = client_socket.recv(1024).decode('utf-8')
-        first_line = client_data.splitlines()[0]
-        path = first_line.split(" ")[1]
-        content = capture_final_path(unquote(path))
-        http_response = generate_content('1.1', '200 OK', 'text/plain', content)
-        print(http_response)
-        client_socket.sendall(http_response.encode())
-        client_socket.close()
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
+
+    server_socket.listen()
+    client_socket, address = server_socket.accept()
+    client_data = client_socket.recv(1024).decode('utf-8')
+    first_line = client_data.splitlines()[0]
+    path = first_line.split(" ")[1]
+    content = capture_final_path(unquote(path))
+    http_response = generate_content('1.1', '200 OK', 'text/plain', content)
+    print(http_response)
+    client_socket.sendall(http_response.encode())
+    client_socket.close()
 
 
 if __name__ == "__main__":
