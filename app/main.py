b4f0822d1d85ab907e98714bfcadcece7214a70c
Douglas Jorge <73705447+xZaR3y4p@users.noreply.github.com>
2023-11-13 19:20:32 -0300

    pass 7th stage #first try

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 # Uncomment this to pass the first stage
 import socket
 import re
-from time import sleep
+import argparse
+import os.path
 from threading import Thread
 from urllib.parse import unquote
 
@@ -22,13 +23,31 @@ def capture_final_path(path):
     match = re.search(pattern, path)
     return match.group(1) if match else ''
 
+
 def get_user_agent(client_data):
     print(client_data)
     pattern = r'User-Agent: (.*)'
     match = re.search(pattern, client_data)
     return match.group(1).strip() if match else ''
 
-def process_socket(client_socket):
+
+def get_file_name(client_data):
+    pattern = r'GET /files/(.*)'
+    match = re.search(pattern, client_data)
+    return match.group(1) if match else ''
+
+
+def check_file(file_name, directory):
+    full_path = os.path.join(directory, file_name)
+    if os.path.isfile(full_path):
+        f = open(full_path, mode='rb')
+        file_content = f.read()
+        f.close()
+        return True, file_content
+    return False, None
+
+
+def process_socket(client_socket, args):
     client_data = client_socket.recv(1024).decode('utf-8')
     first_line = client_data.splitlines()[0]
     path = unquote(first_line.split(" ")[1])
@@ -40,20 +59,32 @@ def process_socket(client_socket):
     elif path == '/user-agent':
         user_agent = get_user_agent(client_data)
         http_response = generate_content('1.1', '200 OK', 'text/plain', user_agent)
+    elif path.startswith('/files'):
+        file_name = get_file_name(client_data)
+        file_exists, file_content = check_file(file_name, args.directory)
+        if file_exists:
+            http_response = generate_content('1.1', '200 OK', 'application/octet', file_content)
+        else:
+            http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
     else:
         http_response = "HTTP/1.1 404 Not Found\r\n\r\n"
     print(http_response)
     client_socket.sendall(http_response.encode())
     client_socket.close()
 
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--directory', type=str, help='folder to look for files')
+    args = parser.parse_args()
+
     server_socket = socket.create_server(("localhost", 4221), reuse_port=False)
     server_socket.listen()
     while True:
         client_socket, address = server_socket.accept()
-        thread = Thread(target=process_socket, args=[client_socket])
+        thread = Thread(target=process_socket, args=[client_socket, args])
         thread.start()
 
 
